12f0d032288b1fe318cd143d7e9c51bb5eeecd0b
Waalfegor <100232625+Waalfegor@users.noreply.github.com>
2022-05-05 15:35:47 +0300

    Add files via upload

---
+++ HW12/MyArr.h
@@ -0,0 +1,15 @@
+#ifndef MYARR_H_
+#define MYARR_H_
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+void FillArr(T* Arr, int n);
+template <class T>
+void OutArr(T* Arr, int n);
+template <class T>
+void bubbleSort(T* Arr, int n);
+
+#endif 
---
+++ HW12/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "MyArr.cpp"
+
+int main()
+{
+	srand(time(0));
+	int size = rand() % 10 + 5;
+	double* Arr = new double[size];
+	FillArr(Arr, size);
+	cout << "Your array: \n";
+	OutArr(Arr, size);
+	cout << "Sorted array: \n";
+	bubbleSort(Arr, size);
+	OutArr(Arr, size);
+	delete[] Arr;
+}
+
+
+
+
